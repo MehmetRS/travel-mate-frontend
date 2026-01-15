10830bc395a6d4cebc0d703830605d92ee6a7a7d
Mehmet Rahim Siyirdici <mrsiyirdici.222@gmail.com>
2026-01-15 15:58:17 +0300

    pages.tsx params fixing

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -1,7 +1,13 @@
 import TripDetailClient from './TripDetailClient';
 
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
 export const dynamicParams = true;
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: PageProps) {
   return <TripDetailClient id={params.id} />;
 }
